7dd5ad66f8843730659760f1e39691abbaa3c6a6
Tommy.Cirilo <tommy.v.cirilo@gmail.com>
2020-09-01 22:42:21 -0500

    finished exercise

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,42 +1,19 @@
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.jupiter.api.DisplayName;
-
-import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class StudentTest {
 
-   /* @Before
-    public void setUp() {
-        this.grades = new ArrayList<>();
-        this.grades.add(99);
+    @Test
+    public void testCreateStudent() {
+        Student fer = new Student(1L, "fer");
+        Student ryan = null;
+        assertNull(ryan);
+        assertNotNull(fer);
     }
 
     @Test
-    @DisplayName("This is the test that sets both the name and id property, and " +
-            "initializes the grades property as an empty array list")
-    public void setNameandId () {
-        String studentName;
-        long studentId;
-        assertNotNull(this.grades);
-    }
-
-    @Test
-    public void initializeGrades() {
-
-    }*/
-   @Test
-   public void testCreateStudent(){
-       Student fer = new Student(1L, "fer");
-       Student ryan = null;
-       assertNull(ryan);
-       assertNotNull(fer);
-   }
-
-    @Test
-    public void testStudentFields(){
+    public void testStudentFields() {
         Student fer = new Student(1L, "fer");
         assertSame(1L, fer.getId());
         assertSame("fer", fer.getName());
@@ -45,7 +22,7 @@ public class StudentTest {
 
 
     @Test
-    public void testAddGrade(){
+    public void testAddGrade() {
         Student fer = new Student(1L, "fer");
         fer.addGrade(100);
         assertSame(100, fer.getGrades().get(0));
@@ -54,14 +31,10 @@ public class StudentTest {
     }
 
     @Test
-    public void testAverageGrade(){
+    public void testAverageGrade() {
         Student fer = new Student(1L, "fer");
         fer.addGrade(100);
         fer.addGrade(80);
         assertEquals(90, fer.getGradeAverage(), 0);
     }
-
-
-
-
 }
